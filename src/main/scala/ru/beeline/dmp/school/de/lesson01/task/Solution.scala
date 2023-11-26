3160c967c9c4c1b774665134a217065f75a9a81c
antonio-st@yandex.ru <antonio-st@yandex.ru>
2023-11-26 17:58:11 +0300

    Complete 1 task

--- src/main/scala/ru/beeline/dmp/school/de/lesson01/task/Solution.scala
+++ src/main/scala/ru/beeline/dmp/school/de/lesson01/task/Solution.scala
@@ -1,35 +1,42 @@
 package ru.beeline.dmp.school.de.lesson01.task
 
 import org.apache.spark.sql.SparkSession
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.internal.Logging
 
-object Solution extends App {
+object Solution extends App with Logging {
 
-	// подключаемся к Spark --------------------------------------------------------------------------------------------
-	val spark: SparkSession = SparkSession
-		.builder()
-		.appName("School_DE_Lesson_01_Task")
-		.enableHiveSupport()
-		.getOrCreate()
+  Logger.getRootLogger.setLevel(Level.WARN)
+  val logger: Logger = Logger.getLogger(getClass.getName)
+  logger.setLevel(Level.INFO)
+  Logger.getLogger("org").setLevel(Level.ERROR)
+
+  // подключаемся к Spark --------------------------------------------------------------------------------------------
+  val spark: SparkSession = SparkSession
+    .builder()
+    .appName("School_DE_Lesson_01_Task")
+    .enableHiveSupport()
+    .getOrCreate()
 
 
-	/** Метод печатает в консоли значение autoBroadcastJoinThreshold */
-	def printConfigValue(spark: SparkSession): Unit = {
+  /** Метод печатает в консоли значение autoBroadcastJoinThreshold */
+  def printConfigValue(spark: SparkSession): Unit = {
 
-		println(spark.conf.get("spark.sql.autoBroadcastJoinThreshold"))
+    println(spark.conf.get("spark.sql.autoBroadcastJoinThreshold"))
 
-	}
+  }
 
-	/** Метод изменяет значение autoBroadcastJoinThreshold */
-	def changeConfigValue(spark: SparkSession, value: String): Unit = {
+  /** Метод изменяет значение autoBroadcastJoinThreshold */
+  def changeConfigValue(spark: SparkSession, value: String): Unit = {
 
-		spark.conf.set("spark.sql.autoBroadcastJoinThreshold", value)
+    spark.conf.set("spark.sql.autoBroadcastJoinThreshold", value)
 
-	}
+  }
 
-	printConfigValue(spark)
+  printConfigValue(spark)
 
-	changeConfigValue(spark, "20mb")
+  changeConfigValue(spark, "20mb")
 
-	printConfigValue(spark)
+  printConfigValue(spark)
 
 }
--- src/main/scala/ru/beeline/dmp/school/de/lesson01/task/Task.scala
+++ src/main/scala/ru/beeline/dmp/school/de/lesson01/task/Task.scala
@@ -1,30 +1,60 @@
 package ru.beeline.dmp.school.de.lesson01.task
 
 import org.apache.spark.sql.SparkSession
-
-object Task extends App {
-
-	// подключаемся к Spark --------------------------------------------------------------------------------------------
-	val spark: SparkSession = SparkSession
-		.builder()
-		.appName("School_DE_Lesson_01_Task")
-		.enableHiveSupport()
-		.getOrCreate()
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.internal.Logging
+import scala.util.Try
 
 
-	/** Метод печатает в консоли значение autoBroadcastJoinThreshold */
-	def printConfigValue(spark: SparkSession): Unit = {
-		???
-	}
+object Task extends App with Logging {
 
-	/** Метод изменяет значение autoBroadcastJoinThreshold */
-	def changeConfigValue(spark: SparkSession, newValue: String): Unit = {
-		???
-	}
+  Logger.getRootLogger.setLevel(Level.WARN)
+  val logger: Logger = Logger.getLogger(getClass.getName)
+  logger.setLevel(Level.INFO)
+  Logger.getLogger("org").setLevel(Level.ERROR)
+
+  // подключаемся к Spark --------------------------------------------------------------------------------------------
+  val spark: SparkSession = SparkSession
+    .builder()
+    .appName("School_DE_Lesson_01_Task")
+    .enableHiveSupport()
+    .getOrCreate()
 
 
-	printConfigValue(spark)
-	changeConfigValue(spark, "100mb")
-	printConfigValue(spark)
+  /** Метод печатает в консоли значение autoBroadcastJoinThreshold */
+  def printConfigValue(spark: SparkSession): Unit = {
+    val valueConf =
+      ((spark.conf.get("spark.sql.autoBroadcastJoinThreshold").split("b")(0).toInt) / 1048576)
 
+    println(s"${valueConf} Mb")
+
+  }
+
+  /** Метод печатает в консоли значение Shuffle Partitions */
+  def printConfigShufflePartitions(spark: SparkSession): Unit = {
+    val valueConf =
+      spark.conf.get("spark.sql.shuffle.partitions")
+    println(valueConf)
+  }
+
+
+  /** Метод изменяет значение autoBroadcastJoinThreshold */
+  def changeConfigValue(spark: SparkSession, newValue: String): Unit = {
+    spark.conf.set("spark.sql.autoBroadcastJoinThreshold", newValue)
+  }
+
+  /** Метод изменяет значение Shuffle Partitions */
+  def changeConfigShufflePartitions(spark: SparkSession, value: Int): Unit = {
+    val valueConf =
+      spark.conf.set("spark.sql.shuffle.partitions", value)
+  }
+
+  printConfigValue(spark)
+  changeConfigValue(spark, "104857600b")
+  printConfigValue(spark)
+
+
+  printConfigShufflePartitions(spark)
+  changeConfigShufflePartitions(spark, 10)
+  printConfigShufflePartitions(spark)
 }
